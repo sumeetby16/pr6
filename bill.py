7f475ea5c715af729346ef11e64cdc6b4dc69562
sumeetby16 <sumeetby16@gmail.com>
2025-10-23 16:24:04 +0530

    Add bill calculation function

---
+++ bill.py
@@ -0,0 +1,15 @@
+
+def calculate_bill(units):
+    rate_per_unit = 5
+    total_bill = units * rate_per_unit
+    discount = 0
+
+    if total_bill > 1000:
+        discount = total_bill * 0.10
+
+    final_bill = total_bill - discount
+
+    print(f"Units Consumed: {units}")
+    print(f"Total Bill: ₹{total_bill}")
+    print(f"Discount Applied: ₹{discount}")
+    print(f"Final Bill: ₹{final_bill}")
